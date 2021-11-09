30b329bce5b3742b198771a9f48a0b964724e5db
Fonsii <lualfonso14200174@gmail.com>
2021-11-09 10:51:31 -0600

    login and navbar

--- web_blog/web_blog/urls.py
+++ web_blog/web_blog/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html")),
+    path('', include("user_app.urls")),
     path('accounts/', include('allauth.urls')),
-    path('logout', LogoutView.as_view()),
 ]
