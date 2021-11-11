b2b2d67e9fe3cd41d27c77bb9dc547c9e8262829
Fonsii <lualfonso14200174@gmail.com>
2021-11-11 11:04:44 -0600

    :)

--- web_blog/admin_app/views/AdministrationView.py
+++ web_blog/admin_app/views/AdministrationView.py
@@ -1,6 +1,8 @@
 from django.views import generic
-
+from django.shortcuts import render
 
 class AdministrationView(generic.View):
     template_name = 'admin_app/administration.html'
-    pass
+    
+    def get(self, request):
+        return render(request, self.template_name)
