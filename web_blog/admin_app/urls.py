6dc3608265ec8344226b2cfa7093d347cdd4961a
Alonso Moreno <jesus.moreno@ucr.ac.cr>
2021-11-12 11:55:32 -0600

    Crud completo de categorías.

--- web_blog/admin_app/urls.py
+++ web_blog/admin_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('administracion/categorias', views.AdministrationView.as_view(), name='administration_categories'),
+    path(r'administracion/categorias/<int:category>',
+         views.AdministrationView.as_view(), name='administration_categories'),
 ]
